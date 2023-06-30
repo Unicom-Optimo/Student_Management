ceb4f2ee81c2c444122313941cbe31a206bb360a
Akalya Gnanasekaram <akalya.gnanasekaram@gmail.com>
2023-06-30 21:07:48 +0530

    Add canActive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,16 @@ import { StudentsComponent } from './students/students.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { AuthGuardServiceService } from './service/auth-guard-service.service';
 
 
 const routes: Routes = [
   { path: '',redirectTo:'login', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'course', component: CourseComponent },
-  { path: 'students', component: StudentsComponent },
+  { path: 'home', component: HomeComponent,canActivate:[AuthGuardServiceService] },
+  { path: 'course', component: CourseComponent ,canActivate:[AuthGuardServiceService] },
+  { path: 'students', component: StudentsComponent,canActivate:[AuthGuardServiceService] },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent ,canActivate:[AuthGuardServiceService] }
 ];
 
 @NgModule({
