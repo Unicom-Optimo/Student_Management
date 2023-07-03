d41f39faed9eefdd30325fd0cdcee8ca316e1e01
Akalya Gnanasekaram <akalya.gnanasekaram@gmail.com>
2023-07-03 11:15:49 +0530

    auth service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup,Validator, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthGuardServiceService } from '../service/auth-guard-service.service';
 
 @Component({
   selector: 'app-login',
@@ -10,96 +11,36 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-public loginForm!:FormGroup
+  public loginForm!: FormGroup
 
-  // formdata={userName:"",password:""}
-  // submit=false;
- 
-  //userName:any
-  //password:any
-/*
-  loginFormValue !: FormGroup;
-  loginModelObj: LoginModel = new LoginModel();
-  loginData !: any; //get
-
-*/
-  constructor(private formBuider:FormBuilder,private http:HttpClient,private router:Router){
+  constructor(private formBuider: FormBuilder, private http: HttpClient, private router: Router,private authService:AuthGuardServiceService) {
 
   }
   ngOnInit(): void {
-    this.loginForm=this.formBuider.group({
-      userName:['',Validators.required],
-      password:['',Validators.required]
+    this.loginForm = this.formBuider.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
     })
   }
-//
-
-  login(){
-this.http.get<any>("https://localhost:7253/api/UserModels")//http://localhost:3000/signupUsers
-.subscribe(res=>{
-  const user=res.find((a:any)=>{
-    return a.userName===this.loginForm.value.userName && a.password===this.loginForm.value.password
-  });
-  if(user){
-    alert("Login Success");
-    this.loginForm.reset();
-    this.router.navigate(['home'])
-  }else{
-    alert("User not found \nPlease enter correct username & password")
-  }
-},err=>{
-  alert("Something went wrong!")
-})
-  }
-  
-  /*##private formbuilder: FormBuilder,
-    private api: ApiServiceLogin) {
-  }*/
 
 
-  
-
-
-//   onSubmit(){
-// console.log(this.formdata)
-// }
-
-
-//   postLgin() {
-//     if (this.formdata.userName == 'AK97' && this.formdata.password == '1234') {
-//       alert("Login Success!");
-//       this.router.navigate(['home']);
-      
-//     }
-//     else {
-//       alert("Login Faild!")
-//     }
-//   }
-
-
-
-
-  //post
-  /*
-  postLgin() {
-
-    this.loginModelObj.userName = this.loginFormValue.value.userName;
-    this.loginModelObj.password = this.loginFormValue.value.password;
-
-
-    this.api.postLogin(this.loginModelObj)
+  login() {
+    this.http.get<any>("https://localhost:7253/api/UserModels")//http://localhost:3000/signupUsers
       .subscribe(res => {
-        console.log(res);
-        alert("Login Successfully")
-        let ref = document.getElementById('cancel')
-        ref?.click();
-        this.loginFormValue.reset();
-      },
-        err => {
-          alert("Something went wrong")
+        const user = res.find((a: any) => {
+          return a.userName === this.loginForm.value.userName && a.password === this.loginForm.value.password
+        });
+        if (user) {
+          alert("Login Success");
+          this.loginForm.reset();
+          this.router.navigate(['home'])
+        } else {
+          alert("User not found \nPlease enter correct username & password")
         }
-      )
-  }*/
+      }, err => {
+        alert("Something went wrong!")
+      })
+  }
 
 
 }
--- src/app/service/auth-guard-service.service.ts
+++ src/app/service/auth-guard-service.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardServiceService implements CanActivate   {
 
-  constructor() { }
+export class AuthGuardServiceService implements CanActivate {
+  someCondition: true;
+  
+  constructor(private_router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-	  return false;
+
+    //check some condition  
+    if (this.someCondition) {
+      alert('You are not allowed to view this page');
+      //redirect to login/home page etc
+      //return false to cancel the navigation
+      return false;
+    }
+    return true;//false;
   }
 }
